4e613786b9045ac570edef3a5874e9c5cbb23784
Wuixe <guillaume.renel@gmail.com>
2023-02-09 22:03:20 +0100

    Map and state addImputButton

--- budget-calculator-React/src/components/forms/addImputButton.js
+++ budget-calculator-React/src/components/forms/addImputButton.js
@@ -5,27 +5,41 @@ class AddImputButton extends Component {
 constructor(props) {
     super(props);
     this.state = {
-        inputName: '',
+        inputName: ['input0'],
         inputState: 0,
+        inputNumber : 0
     }
 }
 
-switchState = () => this.setState({inputState: this.state.inputState + 1});
-            
+switchState = () => {
+    if (this.state.inputState === 2) {
+        this.setState(prevState => {
+            return {
+              inputNumber: prevState.inputNumber + 1,
+              inputName: [...prevState.inputName, 'input' + (Number(prevState.inputNumber) + 1)],
+              inputState : 0
+            };
+          }); 
+    }
+    else {
+        this.setState({inputState: this.state.inputState + 1});
+    }
+    
+}        
 
-render() {
-
+inputCycle = () => {
     const inputState = this.state.inputState;
     
     switch(inputState) {
         case 0:
             return (
                 <div>
-                <button type="button" onClick={this.switchState}>next</button>;
-                <p>case 0</p>
-                <p>{JSON.stringify(this.state)}</p>
+                  <button type="button" onClick={this.switchState}>next</button>
+                  <p>case 0</p>
+                  <p>{JSON.stringify(this.state)}</p>
                 </div>
-            ) 
+              ) 
+               
         case 1:
             return (
                 <div>
@@ -51,7 +65,19 @@ render() {
                 </div>
             ) 
     }
+
 }
+
+render() {
+    return (
+      <div>
+        {this.state.inputName.map(input => (
+          <this.inputCycle key={input} />
+        ))}
+      </div>
+    );
+  }
+  
 
 
 }
